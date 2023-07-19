5d0fb60e3f94a010d115d32fb863676c5c9e36b7
gongzhen <517441540@qq.com>
2023-07-19 16:37:16 +0800

    feat: add three store

---
+++ src/store/modules/three.ts
@@ -0,0 +1,76 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import type { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import type { TransformControls } from 'three/examples/jsm/controls/TransformControls.js'
+
+enum ControlsItemType {
+  orbitControls = 'orbitControls',
+  transformControls = 'transformControls',
+}
+
+enum CameraItemType {
+  perspectiveCamera = 'perspectiveCamera',
+}
+
+type ControlsValueType<T> = T extends ControlsItemType.orbitControls ? OrbitControls : TransformControls
+type CameraValueType<T> = T extends CameraItemType.perspectiveCamera ? PerspectiveCamera : unknown
+
+interface ControlsType {
+  orbitControls?: OrbitControls
+  transformControls?: TransformControls
+}
+
+interface CameraType {
+  perspectiveCamera?: PerspectiveCamera
+}
+
+const useThreeStore = defineStore('threeStore', () => {
+  const scene = ref<null | Scene>(null)
+  const renderer = ref<null | WebGLRenderer>(null)
+  const controls = ref< ControlsType | null>(null)
+  const camera = ref<null | CameraType>(null)
+
+  const setScene = (value: Scene) => {
+    scene.value = value
+  }
+
+  const setRenderer = (value: WebGLRenderer) => {
+    renderer.value = value
+  }
+
+  const setControls = <T extends ControlsItemType> (value: ControlsValueType<T>, key: T) => {
+    if ((controls.value)) {
+      controls.value[key] = value as any
+    }
+    else {
+      controls.value = {
+        [key]: value,
+      }
+    }
+  }
+
+  const setCamera = <T extends CameraItemType>(value: CameraValueType<T>, key: T) => {
+    if ((camera.value)) {
+      camera.value[key] = value as any
+    }
+    else {
+      camera.value = {
+        [key]: value,
+      }
+    }
+  }
+
+  return {
+    scene,
+    renderer,
+    controls,
+    camera,
+    setScene,
+    setRenderer,
+    setControls,
+    setCamera,
+  }
+})
+
+export default useThreeStore
